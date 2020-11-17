4a5ef17ccb3f08b78e65895beb30c72553919c85
LostMikely <micsun0906@gmail.com>
2020-11-17 20:42:42 +0100

    Fixed the behavior of the dropdown buttons.

--- lib/main.dart
+++ lib/main.dart
@@ -25,70 +25,115 @@ class MainAppPage extends StatefulWidget {
 }
 
 class _MainAppPageState extends State<MainAppPage> {
-  String fromCurrency = 'EUR';
-  String toCurrency = 'EUR';
+  Currency fromCurrency = Currency('SEK', 1.00);
+  Currency toCurrency;
+  final amountController = TextEditingController();
 
-  Widget inputSection = Container(
-    padding: const EdgeInsets.all(16),
-    child: Row(
-      children: [
-        Expanded(
-          child: TextField(
-            decoration: InputDecoration(
-              labelText: 'Amount',
-            ),
-          ),
-        ),
-        CurrencyDropdownWidget(),
-        CurrencyDropdownWidget(),
-      ],
-    )
-  );
+  @override
+  void dispose() {
+    // Clean up the controller when the widget is disposed.
+    amountController.dispose();
+    super.dispose();
+  }
+
+  void updateFromCurrency(Currency currency) => setState(() => amountController.text = currency.name);
+  void updateToCurrency(Currency currency) => setState(() => toCurrency = currency);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Currency Converter'),
-        ),
-        body: Column(
+      appBar: AppBar(
+        title: Text('Currency Converter'),
+      ),
+      body: Container(
+        margin: EdgeInsets.all(16),
+        child: Row(
           children: [
-            inputSection,
+            Expanded(
+              child: TextField(
+                controller: amountController,
+                decoration: InputDecoration(
+                  labelText: 'Amount',
+                ),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(left: 8),
+              child: CurrencyDropdown(
+                onCurrencyChanged: updateFromCurrency,
+                hint: 'From',
+              ),
+            ),
+            Icon(
+              Icons.arrow_right_alt,
+              size: 32,
+              color: Colors.grey[700],
+            ),
+            Container(
+              margin: EdgeInsets.only(left: 8),
+              child: CurrencyDropdown(
+                onCurrencyChanged: updateToCurrency,
+                hint: 'To',
+              ),
+            ),
           ],
         ),
+      ),
     );
   }
 }
 
-class CurrencyDropdownWidget extends StatefulWidget {
-  CurrencyDropdownWidget({Key key, String value}) : super(key: key);
+typedef void CurrencyCallback(Currency currency);
+
+class CurrencyDropdown extends StatefulWidget {
+  final CurrencyCallback onCurrencyChanged;
+  final String hint;
+
+  CurrencyDropdown({Key key, @required this.onCurrencyChanged, @required this.hint}) : super(key: key);
 
   @override
-  _CurrencyDropdownState createState() => _CurrencyDropdownState();
+  _CurrencyDropdownState createState() => _CurrencyDropdownState(onCurrencyChanged: this.onCurrencyChanged, hint: this.hint);
 }
 
-class _CurrencyDropdownState extends State<CurrencyDropdownWidget> {
-  String dropdownValue = 'EUR';
+class _CurrencyDropdownState extends State<CurrencyDropdown> {
+  final CurrencyCallback onCurrencyChanged;
+  final String hint;
+
+  // 'GBP', 'CNY', 'JPY', 'KRW'
+  static Currency curEur = Currency('EUR', 0.19);
+  static Currency curSek = Currency('SEK', 1.00);
+  static Currency curUsd = Currency('USD', 0.20);
+  static List<Currency> currencies = [curEur, curSek, curUsd];
+  Currency selectedCurrency = curEur;
+
+  _CurrencyDropdownState({@required this.onCurrencyChanged, @required this.hint});
+
 
   @override
   Widget build(BuildContext context) {
-    return DropdownButton<String>(
-      value: dropdownValue,
+    return DropdownButton<Currency>(
+      hint: Text(hint),
       icon: Icon(Icons.arrow_downward),
+      value: selectedCurrency,
       iconSize: 24,
       elevation: 16,
-      onChanged: (String newValue) {
-        setState(() {
-          dropdownValue = newValue;
-        });
+      onChanged: (Currency newCurrency) {
+        onCurrencyChanged(newCurrency);
+        selectedCurrency = newCurrency;
       },
-      items: <String>['EUR', 'SEK', 'USD', 'GBP', 'CNY', 'JPY', 'KRW']
-          .map<DropdownMenuItem<String>>((String value) {
-            return DropdownMenuItem<String>(
-              value: value,
-              child: Text(value),
+      items: currencies.map((Currency currency) {
+            return DropdownMenuItem<Currency>(
+              value: currency,
+              child: Text(currency.name),
             );
       }).toList(),
     );
   }
+}
+
+class Currency {
+  final String name;
+  final double rate;
+
+  const Currency(this.name, this.rate);
 }
